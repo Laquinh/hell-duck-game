dd9e0d23b4f849724e2848406e3ae5640f7007c5
Laquin <anderaginaga2@gmail.com>
2025-10-19 00:17:28 +0200

    Make possessed furniture dance

--- player/player.gd
+++ player/player.gd
@@ -3,8 +3,11 @@ extends CharacterBody2D
 @export var speed: float = 200
 @export var acceleration: float = 1000
 @export var friction: float = 2200
+@export var dance_amplitude: float = 4
+@export var dance_speed: float = 5
 
 var possessed_furniture: Node2D = null
+var dance_time: float = 0
 
 func _physics_process(delta: float) -> void:
 	var input = Vector2.ZERO
@@ -29,10 +32,14 @@ func _physics_process(delta: float) -> void:
 	
 	if possessed_furniture:
 		possessed_furniture.position = position
+		dance_time += delta * dance_speed
+		possessed_furniture.rotation_degrees = dance_amplitude * sin(dance_time)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("possess"):
-		if possessed_furniture:   
+		if possessed_furniture:
+			dance_time = 0
+			possessed_furniture.rotation_degrees = 0
 			possessed_furniture = null
 			visible = true      
 		else:
