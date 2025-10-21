4cc7e3cc6d8052b0afb4a40ff853016b8dd39892
Laquin <anderaginaga2@gmail.com>
2025-10-21 17:19:23 +0200

    Implement volume change through slider

--- pause-menu/pause-menu.gd
+++ pause-menu/pause-menu.gd
@@ -1,12 +1,14 @@
 extends CanvasLayer
 
-var is_paused = false
+var bus_index: int
+var is_paused: bool = false
 
 func _input(event: InputEvent) -> void:
+	bus_index = AudioServer.get_bus_index("Master")
 	if event.is_action_pressed("ui_cancel"):
 		toggle_pause()
 
-func toggle_pause():
+func toggle_pause() -> void:
 	is_paused = !is_paused
 	get_tree().paused = is_paused
 	visible = is_paused
@@ -16,3 +18,6 @@ func _on_resume_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
