156a1cb4d556044c9b7a91689a34a0d9cf80d0cb
Laquin <anderaginaga2@gmail.com>
2025-10-25 03:46:57 +0200

    Remove error message that should not appear

--- furniture/furniture.gd
+++ furniture/furniture.gd
@@ -11,7 +11,4 @@ extends Resource
 func _init(p_texture = null, p_interaction_box_padding = Vector2.ZERO, p_furniture_behavior = null):
 	texture = p_texture
 	interaction_box_padding = p_interaction_box_padding
-	if (p_furniture_behavior and p_furniture_behavior.is_class("FurnitureBehavior")):
-		furniture_behavior = p_furniture_behavior
-	else:
-		push_error("Script must extend FurnitureBehavior")
+	furniture_behavior = p_furniture_behavior
