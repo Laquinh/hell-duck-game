8828153712b2aa688687b337ffa46127d89a2da9
Laquin <anderaginaga2@gmail.com>
2025-10-25 21:56:34 +0200

    Handle fainting after seeing all scary furniture

--- npc/npc-behavior.gd
+++ npc/npc-behavior.gd
@@ -6,4 +6,8 @@ func _physics_process(delta: float) -> void:
 		if area.is_in_group("Furniture"):
 			var npc_initialiser: NpcInitialiser = get_parent()
 			if area in npc_initialiser.scary_furniture:
-				print("AAAAAAAAAAAAHHH")
+				npc_initialiser.scary_furniture.erase(area)
+				if npc_initialiser.scary_furniture.size() > 0:
+					print("AAAAAAAAAAAAHHH")
+				else:
+					print("I can't take this anymore")
