dc4918ffbf7221cf639edb27ecdeeae21731ad5a
Laquin <anderaginaga2@gmail.com>
2025-10-25 11:52:31 +0200

    Implement basic scaring

--- npc/npc-behavior.gd
+++ npc/npc-behavior.gd
@@ -1,2 +1,9 @@
 class_name NpcBehavior
 extends Area2D
+
+func _physics_process(delta: float) -> void:
+	for area in get_parent().get_overlapping_areas():
+		if area.is_in_group("Furniture"):
+			var npc_initialiser: NpcInitialiser = get_parent()
+			if area in npc_initialiser.scary_furniture:
+				print("AAAAAAAAAAAAHHH")
