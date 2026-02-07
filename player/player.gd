39cbbc20d58347a0e43dc3f0ab2772d588e1955b
Yari 3 <129322461+Yari-Pixels@users.noreply.github.com>
2026-02-07 01:01:07 +0100

    Updates project to godot version 4.6 and ads collision with walls.

--- player/player.gd
+++ player/player.gd
@@ -9,7 +9,13 @@ extends CharacterBody2D
 @export var acceleration: float = 1000
 @export var friction: float = 2200
 
+var current_collider: CollisionShape2D = null
+var original_collider_size: Vector2 = Vector2.ZERO
 var possessed_furniture: FurnitureBehavior = null
+
+func _ready():
+	current_collider = $CollisionShape2D
+	original_collider_size = current_collider.shape.size
 
 func _physics_process(delta: float) -> void:
 	var input = Vector2.ZERO
@@ -47,9 +53,11 @@ func _input(event: InputEvent) -> void:
 			possessed_furniture.toggle_possessed()
 			possessed_furniture = null
 			visible = true
+			collision_mask &= ~(1 << 0)
+			current_collider.shape.size = original_collider_size
 		else:
 			var potential_furniture = null
-			var potential_distance = null
+			var potential_distance: float = INF
 			print(1)
 			for area in $Area2D.get_overlapping_areas():
 				print(2)
@@ -57,16 +65,21 @@ func _input(event: InputEvent) -> void:
 					print(3)
 					if !potential_furniture:
 						print(4)
-						potential_furniture = area.behavior
+						potential_furniture = area
 						potential_distance = global_position.distance_to(area.global_position)
 					else:
 						print(5)
 						var new_distance = global_position.distance_to(area.global_position)
 						if new_distance < potential_distance:
 							print(6)
-							potential_furniture = area.behavior
+							potential_furniture = area
 							potential_distance = new_distance
 			if potential_furniture:
-				possessed_furniture = potential_furniture
+				possessed_furniture = potential_furniture.behavior
 				possessed_furniture.toggle_possessed()
 				visible = false
+				collision_mask |= 1 << 0
+				var furniture: Furniture = potential_furniture.get("furniture")
+				if furniture:
+					if potential_furniture.furniture and potential_furniture.furniture.texture:
+						current_collider.shape.size = potential_furniture.furniture.texture.get_size()
